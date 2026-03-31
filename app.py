6db39095e5f10186eef011cbeb11b75ea6ed2bed
Aditya Rai <adi@Adityas-MacBook-Air.local>
2026-03-31 23:36:49 +0530

    final test

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
-from flask import Flask, render_template, request
-from weather import get_weather
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
-    weather = None
-
-    if request.method == "POST":
-        city = request.form.get("city")
-        if city:
-            weather = get_weather(city)
-
-    return render_template("index.html", weather=weather)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return "HELLO BHAI WORKING 🔥"
